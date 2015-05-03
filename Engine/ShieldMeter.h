004d3c23bedbf078183f9f8998f13b1dbd139e76
planetchili <planetchili@users.noreply.github.com>
2015-05-03 22:11:17 +0900

    fix color and length of meter

--- Engine/ShieldMeter.h
+++ Engine/ShieldMeter.h
@@ -14,8 +14,8 @@ private:
 		virtual void Rasterize( D3DGraphics& gfx ) const override
 		{
 			const Color meterColor = 
-				{ 255,unsigned char( 255 * ( 1.0f - parent.ship.GetShieldPercent() ) ),
-				unsigned char( 255 * ( 1.0f - parent.ship.GetShieldPercent() ) ) };
+				{ 255,unsigned char( 255 * parent.ship.GetShieldPercent() ),
+				unsigned char( 255 * parent.ship.GetShieldPercent() ) };
 			const int meterWidth = int( ( ( parent.region.right - parent.region.left ) -
 				( 2 * parent.borderWidth + 2 * parent.spaceWidth ) ) * parent.ship.GetShieldPercent() );
 			gfx.DrawRectangle( parent.region.left,parent.region.right,
@@ -30,7 +30,7 @@ private:
 			gfx.DrawRectangle( parent.region.left,parent.region.right,
 				parent.region.bottom - (parent.borderWidth - 1),parent.region.bottom,parent.borderColor );
 			gfx.DrawRectangle( parent.region.left + parent.borderWidth + parent.spaceWidth,
-				parent.region.left + parent.borderWidth + parent.spaceWidth + meterWidth,
+				parent.region.left + parent.borderWidth + parent.spaceWidth + meterWidth - 1,
 				parent.region.top + parent.borderWidth + parent.spaceWidth,
 				parent.region.bottom - ( parent.borderWidth + parent.spaceWidth ),meterColor );
 		}
