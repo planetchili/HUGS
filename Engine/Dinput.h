21cbe050aa22975e3fb766095b9b9730e424936c
planetchili <planetchili@users.noreply.github.com>
2015-06-24 13:00:15 +0900

    dinput gamepad classes added

---
+++ Engine/Dinput.h
@@ -0,0 +1,321 @@
+#pragma once
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "dinput8.lib")
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+#include <memory>
+#include <vector>
+#include <string>
+#include "ChiliMath.h"
+#include "Vec2.h"
+#include <array>
+#include <assert.h>
+#include <queue>
+
+class Gamepad
+{
+public:
+	enum StickType : int
+	{
+		Dpad = 0,
+		Analog1 = 1,
+		Analog2 = 2
+	};
+	class Event
+	{
+	public:
+		enum Type
+		{
+			Button,
+			Axis,
+			Stick,
+			Invalid
+		};
+	public:
+		Event( int index,Vec2 pos )
+			:
+			index( index ),
+			pos( pos ),
+			type( Stick )
+		{}
+		Event( float val )
+			:
+			index( -1 ),
+			val( val ),
+			type( Axis )
+		{}
+		Event( int index,bool pressed )
+			:
+			index( index ),
+			pressed( pressed ),
+			type( Button )
+		{}
+		Event()
+			:
+			type( Invalid ),
+			index( -1 ),
+			pressed( false )
+		{}
+		float GetAxisVal() const
+		{
+			assert( type == Axis );
+			return val;
+		}
+		int GetIndex() const
+		{
+			assert( type == Button || type == Stick );
+			return index;
+		}
+		bool IsPressed() const
+		{
+			assert( type == Button );
+			return pressed;
+		}
+		Vec2 GetStickPos() const
+		{
+			assert( type == Stick );
+			return pos;
+		}
+		bool IsValid() const
+		{
+			return type != Invalid;
+		}
+		Type GetType() const
+		{
+			return type;
+		}
+	private:
+		const Type type;
+		const int index;
+		const union
+		{
+			struct
+			{
+				Vec2 pos;
+			};
+			float val;
+			bool pressed;
+		};
+	};
+public:
+	Gamepad( IDirectInput8W* pInput,HWND hWnd,const GUID& guid )
+	{
+		pInput->CreateDevice( guid,&pDev,nullptr ); 
+		pDev->SetDataFormat( &c_dfDIJoystick2 );
+		{
+			DIPROPDWORD prop;
+			prop.diph.dwHeaderSize = sizeof( prop.diph );
+			prop.diph.dwSize = sizeof( prop );
+			prop.diph.dwHow = DIPH_DEVICE;
+			prop.diph.dwObj = 0;
+			prop.dwData = 256;
+			pDev->SetProperty( DIPROP_BUFFERSIZE,&prop.diph );
+		}
+		pDev->EnumObjects( EnumObjectsCallback,this,DIDFT_ALL );
+		pDev->SetCooperativeLevel( hWnd,DISCL_BACKGROUND | DISCL_EXCLUSIVE );
+	}
+	~Gamepad()
+	{
+		if( pDev != nullptr )
+		{
+			pDev->Unacquire();
+			pDev->Release();
+			pDev = nullptr;
+		}
+	}
+	Vec2 GetStickData( int index = 0 ) const
+	{
+		// preset for 1 pov control (0) and 2 analog sticks (1 and 2)
+		assert( index >= Dpad && index <= Analog2 );
+		switch( index )
+		{
+		case Dpad: // pov values set for d-pad / might not work for other objects
+			switch( state.rgdwPOV[0] )
+			{
+			case 0:
+				return { 0.0f,-1000.f };
+			case 4500:
+				return { 1000.0f,-1000.f };
+			case 9000:
+				return { 1000.0f,0.f };
+			case 13500:
+				return { 1000.0f,1000.f };
+			case 18000:
+				return { 0.0f,1000.f };
+			case 22500:
+				return { -1000.0,1000.f };
+			case 27000:
+				return { -1000.0,0.f };
+			case 31500:
+				return { -1000.0,-1000.f };
+			default:
+				return { 0.0f,0.0f };
+			}
+		case Analog1:
+			return { float( state.lX ),float( state.lY ) };
+		case Analog2:
+			return { float( state.lRx ),float( state.lRy ) };
+		default:
+			return { 0.0f,0.0f };
+		}
+	}
+	float GetAxisData() const
+	{
+		return float( state.lZ );
+	}
+	bool GetButtonData( int index ) const
+	{
+		assert( index > 0 && index <= 127 );
+		return (state.rgbButtons[index] & 0x80) == 0x80;
+	}
+	void Update()
+	{
+		pDev->Acquire();
+		pDev->Poll();
+		pDev->GetDeviceState( sizeof( state ),&state );
+
+		DWORD nItems = 20;
+		std::array<DIDEVICEOBJECTDATA,20> data;
+		pDev->GetDeviceData( sizeof( DIDEVICEOBJECTDATA ),data.data(),&nItems,0 );
+		for( unsigned int i = 0; i < nItems; i++ )
+		{
+			if( data[i].dwOfs >= offsetof( DIJOYSTATE2,rgbButtons[0] ) &&
+				data[i].dwOfs <= offsetof( DIJOYSTATE2,rgbButtons[127] ) )
+			{
+				events.emplace( data[i].dwOfs - offsetof( DIJOYSTATE2,rgbButtons[0] ),
+					(data[i].dwData & 0x80) == 0x80 );
+			}
+			else if( data[i].dwOfs == offsetof( DIJOYSTATE2,rgbButtons[0] ) )
+			{
+				Vec2 pos;
+				switch( data[i].dwData )
+				{
+				case 0:
+					pos = { 0.0f,-1000.f };
+				case 4500:
+					pos = { 1000.0f,-1000.f };
+				case 9000:
+					pos = { 1000.0f,0.f };
+				case 13500:
+					pos = { 1000.0f,1000.f };
+				case 18000:
+					pos = { 0.0f,1000.f };
+				case 22500:
+					pos = { -1000.0,1000.f };
+				case 27000:
+					pos = { -1000.0,0.f };
+				case 31500:
+					pos = { -1000.0,-1000.f };
+				default:
+					pos = { 0.0f,0.0f };
+				}
+				events.emplace( Dpad,pos );
+			}
+			else if( data[i].dwOfs == offsetof( DIJOYSTATE2,lX ) ||
+				data[i].dwOfs == offsetof( DIJOYSTATE2,lY ) )
+			{
+				events.emplace( Analog1,Vec2 { float( state.lX ),float( state.lY ) } );
+			}
+			else if( data[i].dwOfs == offsetof( DIJOYSTATE2,lRx ) ||
+				data[i].dwOfs == offsetof( DIJOYSTATE2,lRy ) )
+			{
+				events.emplace( Analog2,Vec2 { float( state.lRx ),float( state.lRy ) } );
+			}
+			else if( data[i].dwOfs == offsetof( DIJOYSTATE2,lZ ) )
+			{
+				events.emplace( float( data[i].dwData ) );
+			}
+			if( events.size() > maxEvents )
+			{
+				events.pop();
+			}
+		}
+	}
+	Event ReadEvent()
+	{
+		Event e = events.front();
+		events.pop();
+		return e;
+	}
+	Event PeekEvent() const
+	{
+		return events.front();
+	}
+	bool IsEmpty() const
+	{
+		return events.empty();
+	}
+private:
+	static BOOL CALLBACK
+		EnumObjectsCallback( const DIDEVICEOBJECTINSTANCE* pdidoi,VOID* pContext )
+	{
+		Gamepad* pPad = reinterpret_cast<Gamepad*>( pContext );
+		if( pdidoi->dwType & DIDFT_AXIS )
+		{
+			DIPROPRANGE diprg;
+			diprg.diph.dwSize = sizeof( DIPROPRANGE );
+			diprg.diph.dwHeaderSize = sizeof( DIPROPHEADER );
+			diprg.diph.dwHow = DIPH_BYID;
+			diprg.diph.dwObj = pdidoi->dwType;
+			diprg.lMin = -1000;
+			diprg.lMax = +1000;
+
+			pPad->pDev->SetProperty( DIPROP_RANGE,&diprg.diph );
+			
+			DIPROPDWORD didw;
+			didw.diph.dwSize = sizeof( DIPROPDWORD );
+			didw.diph.dwHeaderSize = sizeof( DIPROPHEADER );
+			didw.diph.dwHow = DIPH_BYID;
+			didw.diph.dwObj = pdidoi->dwType;
+			didw.dwData = 100;
+			
+			pPad->pDev->SetProperty( DIPROP_DEADZONE,&didw.diph );
+		}
+
+		return DIENUM_CONTINUE;
+	}
+private:
+	const unsigned int maxEvents = 64;
+	DIJOYSTATE2 state;
+	IDirectInputDevice8W* pDev = nullptr;
+	std::queue<Event> events;
+};
+
+class DirectInput
+{
+public:
+	DirectInput( HWND hWnd )
+		:
+		hWnd( hWnd )
+	{
+		DirectInput8Create( GetModuleHandle( NULL ),DIRECTINPUT_VERSION,
+			IID_IDirectInput8,(VOID**)&pInput,NULL );
+
+		pInput->EnumDevices( DI8DEVCLASS_GAMECTRL,DeviceEnumCallback,
+			(VOID*)this,DIEDFL_ATTACHEDONLY );
+	}
+	~DirectInput()
+	{
+		if( pInput != nullptr )
+		{
+			pInput->Release();
+			pInput = nullptr;
+		}
+	}
+	Gamepad& GetPad()
+	{
+		return *pPad;
+	}
+private:
+	static BOOL CALLBACK
+		DeviceEnumCallback( const DIDEVICEINSTANCE* instance,VOID* context )
+	{
+		DirectInput* pInput = static_cast<DirectInput*>( context );
+		pInput->pPad = std::make_unique<Gamepad>( pInput->pInput,pInput->hWnd,instance->guidInstance );
+		return DIENUM_STOP;
+	}
+private:
+	const HWND hWnd;
+	std::unique_ptr<Gamepad> pPad;
+	IDirectInput8W* pInput = nullptr;
+};
