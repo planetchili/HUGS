a2ed78aaebc4f49fd7f95a5d391a591df31a9efd
planetchili <planetchili@users.noreply.github.com>
2015-11-10 13:46:56 +0900

    crazy try-catch bullshit

--- Engine/Sound.h
+++ Engine/Sound.h
@@ -132,103 +132,124 @@ public:
 	{
 		unsigned int fileSize = 0;
 		std::unique_ptr<BYTE[]> pFileIn;
+		try
 		{
-			std::ifstream file( fileName,std::ios_base::binary );
-
 			{
-				int fourcc;
-				file.read( reinterpret_cast<char*>( &fourcc ),4 );
-				if( fourcc != 'FFIR' )
+				std::ifstream file;
+				file.exceptions( std::ifstream::failbit | std::ifstream::badbit );
+				try
 				{
-					throw SoundSystem::FileError( "bad fourCC" );
+					file.open( fileName,std::ios::binary );
+				}
+				catch( std::exception e )
+				{
+					throw SoundSystem::FileError( 
+						std::string( "error opening file: " ) + e.what() );
+				}
+
+				{
+					int fourcc;
+					file.read( reinterpret_cast<char*>( &fourcc ),4 );
+					if( fourcc != 'FFIR' )
+					{
+						throw SoundSystem::FileError( "bad fourCC" );
+					}
+				}
+
+				file.read( reinterpret_cast<char*>( &fileSize ),4 );
+				if( fileSize <= 16 )
+				{
+					throw SoundSystem::FileError( "file too small" );
+				}
+
+				file.seekg( 0,std::ios::beg );
+				pFileIn = std::make_unique<BYTE[]>( fileSize );
+				file.read( reinterpret_cast<char*>( pFileIn.get() ),fileSize );
+			}
+
+			if( *reinterpret_cast<const int*>( &pFileIn[8] ) != 'EVAW' )
+			{
+				throw SoundSystem::FileError( "format not WAVE" );
+			}
+
+			//look for 'fmt ' chunk id
+			WAVEFORMATEX format;
+			bool bFilledFormat = false;
+			for( unsigned int i = 12; i < fileSize; )
+			{
+				if( *reinterpret_cast<const int*>( &pFileIn[i] ) == ' tmf' )
+				{
+					memcpy( &format,&pFileIn[i + 8],sizeof( format ) );
+					bFilledFormat = true;
+					break;
+				}
+				// chunk size + size entry size + chunk id entry size + word padding
+				i += ( *reinterpret_cast<const int*>( &pFileIn[i + 4] ) + 9 ) & 0xFFFFFFFE;
+			}
+			if( !bFilledFormat )
+			{
+				throw SoundSystem::FileError( "fmt chunk not found" );
+			}
+
+			// compare format with sound system format
+			{
+				const WAVEFORMATEX sysFormat = SoundSystem::GetFormat();
+
+				if( format.nChannels != sysFormat.nChannels )
+				{
+					throw SoundSystem::FileError( "bad wave format (nChannels)" );
+				}
+				else if( format.wBitsPerSample != sysFormat.wBitsPerSample )
+				{
+					throw SoundSystem::FileError( "bad wave format (wBitsPerSample)" );
+				}
+				else if( format.nSamplesPerSec != sysFormat.nSamplesPerSec )
+				{
+					throw SoundSystem::FileError( "bad wave format (nSamplesPerSec)" );
+				}
+				else if( format.wFormatTag != sysFormat.wFormatTag )
+				{
+					throw SoundSystem::FileError( "bad wave format (wFormatTag)" );
+				}
+				else if( format.nBlockAlign != sysFormat.nBlockAlign )
+				{
+					throw SoundSystem::FileError( "bad wave format (nBlockAlign)" );
+				}
+				else if( format.nAvgBytesPerSec != sysFormat.nAvgBytesPerSec )
+				{
+					throw SoundSystem::FileError( "bad wave format (nAvgBytesPerSec)" );
 				}
 			}
 
-			file.read( reinterpret_cast<char*>( &fileSize ),4 );
-			if( fileSize <= 16 )
+			//look for 'data' chunk id
+			bool bFilledData = false;
+			for( unsigned int i = 12; i < fileSize; )
 			{
-				throw SoundSystem::FileError( "file too small" );
-			}
+				const int chunkSize = *reinterpret_cast<const int*>( &pFileIn[i + 4] );
+				if( *reinterpret_cast<const int*>( &pFileIn[i] ) == 'atad' )
+				{
+					pData = std::make_unique<BYTE[]>( chunkSize );
+					nBytes = chunkSize;
+					memcpy( pData.get(),&pFileIn[i + 8],nBytes );
 
-			file.seekg( 0,std::ios::beg );
-			pFileIn = std::make_unique<BYTE[]>( fileSize );
-			file.read( reinterpret_cast<char*>( pFileIn.get() ),fileSize );
-		}
-		
-		if( *reinterpret_cast<const int*>( &pFileIn[8] ) != 'EVAW' )
-		{
-			throw SoundSystem::FileError( "format not WAVE" );
-		}
-
-		//look for 'fmt ' chunk id
-		WAVEFORMATEX format;
-		bool bFilledFormat = false;
-		for( unsigned int i = 12; i < fileSize; )
-		{
-			if( *reinterpret_cast<const int*>( &pFileIn[i] ) == ' tmf' )
-			{
-				memcpy( &format,&pFileIn[i + 8],sizeof( format ) );
-				bFilledFormat = true;
-				break;
+					bFilledData = true;
+					break;
+				}
+				// chunk size + size entry size + chunk id entry size + word padding
+				i += ( chunkSize + 9 ) & 0xFFFFFFFE;
 			}
-			// chunk size + size entry size + chunk id entry size + word padding
-			i += ( *reinterpret_cast<const int*>( &pFileIn[i + 4] ) + 9 ) & 0xFFFFFFFE;
-		}
-		if( !bFilledFormat )
-		{
-			throw SoundSystem::FileError( "fmt chunk not found" );
-		}
-
-		// compare format with sound system format
-		{
-			const WAVEFORMATEX sysFormat = SoundSystem::GetFormat();
-
-			if( format.nChannels != sysFormat.nChannels )
+			if( !bFilledData )
 			{
-				throw SoundSystem::FileError( "bad wave format (nChannels)" );
-			}
-			else if( format.wBitsPerSample != sysFormat.wBitsPerSample )
-			{
-				throw SoundSystem::FileError( "bad wave format (wBitsPerSample)" );
-			}
-			else if( format.nSamplesPerSec != sysFormat.nSamplesPerSec )
-			{
-				throw SoundSystem::FileError( "bad wave format (nSamplesPerSec)" );
-			}
-			else if( format.wFormatTag != sysFormat.wFormatTag )
-			{
-				throw SoundSystem::FileError( "bad wave format (wFormatTag)" );
-			}
-			else if( format.nBlockAlign != sysFormat.nBlockAlign )
-			{
-				throw SoundSystem::FileError( "bad wave format (nBlockAlign)" );
-			}
-			else if( format.nAvgBytesPerSec != sysFormat.nAvgBytesPerSec )
-			{
-				throw SoundSystem::FileError( "bad wave format (nAvgBytesPerSec)" );
+				throw SoundSystem::FileError( "data chunk not found" );
 			}
 		}
-
-		//look for 'data' chunk id
-		bool bFilledData = false;
-		for( unsigned int i = 12; i < fileSize; )
+		catch( SoundSystem::Error e )
 		{
-			const int chunkSize = *reinterpret_cast<const int*>( &pFileIn[i + 4] );
-			if( *reinterpret_cast<const int*>( &pFileIn[i] ) == 'atad' )
-			{
-				pData = std::make_unique<BYTE[]>( chunkSize );
-				nBytes = chunkSize;
-				memcpy( pData.get(),&pFileIn[i + 8],nBytes );
-
-				bFilledData = true;
-				break;
-			}
-			// chunk size + size entry size + chunk id entry size + word padding
-			i += ( chunkSize + 9 ) & 0xFFFFFFFE;
+			throw e;
 		}
-		if( !bFilledData )
+		catch( std::exception e )
 		{
-			throw SoundSystem::FileError( "data chunk not found" );
+			throw SoundSystem::FileError( e.what() );
 		}
 	}
 	void Play()
