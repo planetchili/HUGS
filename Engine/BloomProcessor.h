7a1f93348a953e5e559b49705c5b341e76d594bd
planetchili <planetchili@users.noreply.github.com>
2015-12-02 22:33:18 +0900

    analyze difference

--- Engine/BloomProcessor.h
+++ Engine/BloomProcessor.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Surface.h"
 #include "ChiliMath.h"
+#include <immintrin.h>
 
 #define BLOOM_PROCESSOR_USE_SSE
 
@@ -35,53 +36,75 @@ public:
 	}
 	void DownsizePass()
 	{
-		const Color* const pInputBuffer = input.GetBufferConst();
-		Color* const pOutputBuffer = hBuffer.GetBuffer();
-		const size_t inWidth = input.GetWidth();
-		const size_t inHeight = input.GetHeight();
-		const size_t outWidth = hBuffer.GetWidth();
-		const size_t outHeight = hBuffer.GetHeight();
+		// surface height needs to be a multiple of 4
+		assert( input.GetHeight() % 4u == 0u );
 
-		for( size_t y = 0; y < outHeight; y++ )
+		// useful constants
+		const __m128i zero = _mm_setzero_si128();
+
+		// subroutine
+		const auto ProcessRow = [&zero]( __m128i row )
 		{
-			for( size_t x = 0; x < outWidth; x++ )
+			// unpack byte channels of 2 upper and 2 lower pixels to words
+			const __m128i chanLo = _mm_unpacklo_epi8( row,zero );
+			const __m128i chanHi = _mm_unpackhi_epi8( row,zero );
+			
+			// broadcast bloom value to all channels in the same pixel
+			const __m128i bloomLo = _mm_shufflehi_epi16( _mm_shufflelo_epi16( 
+				chanLo,_MM_SHUFFLE( 3u,3u,3u,3u ) ),_MM_SHUFFLE( 3u,3u,3u,3u ) );
+			const __m128i bloomHi = _mm_shufflehi_epi16( _mm_shufflelo_epi16(
+				chanHi,_MM_SHUFFLE( 3u,3u,3u,3u ) ),_MM_SHUFFLE( 3u,3u,3u,3u ) );
+
+			// multiply bloom with color channels
+			const __m128i prodLo = _mm_mullo_epi16( chanLo,bloomLo );
+			const __m128i prodHi = _mm_mullo_epi16( chanHi,bloomHi );
+
+			// predivide channels by 16
+			const __m128i predivLo = _mm_srli_epi16( prodLo,4u );
+			const __m128i predivHi = _mm_srli_epi16( prodHi,4u );
+
+			// add upper and lower 2-pixel groups and return result
+			return _mm_add_epi16( predivLo,predivHi );
+		};
+		
+		for( size_t yIn = 0u,yOut = 0u; yIn < size_t( input.GetHeight() ); yIn += 4u,yOut++ )
+		{
+			// initialize input pointers
+			const __m128i* pRow0 = reinterpret_cast<const __m128i*>(
+				&input.GetBufferConst()[input.GetPitch() * yIn] );
+			const __m128i* pRow1 = reinterpret_cast<const __m128i*>(
+				&input.GetBufferConst()[input.GetPitch() * (yIn + 1u)] );
+			const __m128i* pRow2 = reinterpret_cast<const __m128i*>(
+				&input.GetBufferConst()[input.GetPitch() * (yIn + 2u)] );
+			const __m128i* pRow3 = reinterpret_cast<const __m128i*>(
+				&input.GetBufferConst()[input.GetPitch() * (yIn + 3u)] );
+			// initialize output pointer
+			Color* pOut = &hBuffer.GetBuffer()[hBuffer.GetPitch() * yOut];
+			// row end pointer
+			const __m128i* const pRowEnd = pRow1;
+
+			for( ; pRow0 < pRowEnd; pRow0++,pRow1++,pRow2++,pRow3++,pOut++ )
 			{
-				const Color p0 = pInputBuffer[( y * 4 )     * inWidth + x * 4];
-				const Color p1 = pInputBuffer[( y * 4 )     * inWidth + x * 4 + 1];
-				const Color p2 = pInputBuffer[( y * 4 )     * inWidth + x * 4 + 2];
-				const Color p3 = pInputBuffer[( y * 4 )     * inWidth + x * 4 + 3];
-				const Color p4 = pInputBuffer[( y * 4 + 1 ) * inWidth + x * 4];
-				const Color p5 = pInputBuffer[( y * 4 + 1 ) * inWidth + x * 4 + 1];
-				const Color p6 = pInputBuffer[( y * 4 + 1 ) * inWidth + x * 4 + 2];
-				const Color p7 = pInputBuffer[( y * 4 + 1 ) * inWidth + x * 4 + 3];
-				const Color p8 = pInputBuffer[( y * 4 + 2 ) * inWidth + x * 4];
-				const Color p9 = pInputBuffer[( y * 4 + 2 ) * inWidth + x * 4 + 1];
-				const Color p10 = pInputBuffer[( y * 4 + 2 ) * inWidth + x * 4 + 2];
-				const Color p11 = pInputBuffer[( y * 4 + 2 ) * inWidth + x * 4 + 3];
-				const Color p12 = pInputBuffer[( y * 4 + 3 ) * inWidth + x * 4];
-				const Color p13 = pInputBuffer[( y * 4 + 3 ) * inWidth + x * 4 + 1];
-				const Color p14 = pInputBuffer[( y * 4 + 3 ) * inWidth + x * 4 + 2];
-				const Color p15 = pInputBuffer[( y * 4 + 3 ) * inWidth + x * 4 + 3];
-				const unsigned int x0 = p0.x;
-				const unsigned int x1 = p1.x;
-				const unsigned int x2 = p2.x;
-				const unsigned int x3 = p3.x;
-				const unsigned int x4 = p0.x;
-				const unsigned int x5 = p5.x;
-				const unsigned int x6 = p6.x;
-				const unsigned int x7 = p7.x;
-				const unsigned int x8 = p8.x;
-				const unsigned int x9 = p9.x;
-				const unsigned int x10 = p10.x;
-				const unsigned int x11 = p11.x;
-				const unsigned int x12 = p12.x;
-				const unsigned int x13 = p13.x;
-				const unsigned int x14 = p14.x;
-				const unsigned int x15 = p15.x;
-				pOutputBuffer[y * outWidth + x] =
-				{ unsigned char( ( p0.r * x0 + p1.r * x1 + p2.r * x2 + p3.r * x3 + p4.r * x4 + p5.r * x5 + p6.r * x6 + p7.r * x7 + p8.r * x8 + p9.r * x9 + p10.r * x10 + p11.r * x11 + p12.r * x12 + p13.r * x13 + p14.r * x14 + p15.r * x15 ) / ( 16 * 255 ) ),
-				unsigned char( ( p0.g * x0 + p1.g * x1 + p2.g * x2 + p3.g * x3 + p4.g * x4 + p5.g * x5 + p6.g * x6 + p7.g * x7 + p8.g * x8 + p9.g * x9 + p10.g * x10 + p11.g * x11 + p12.g * x12 + p13.g * x13 + p14.g * x14 + p15.g * x15 ) / ( 16 * 255 ) ),
-				unsigned char( ( p0.b * x0 + p1.b * x1 + p2.b * x2 + p3.b * x3 + p4.b * x4 + p5.b * x5 + p6.b * x6 + p7.b * x7 + p8.b * x8 + p9.b * x9 + p10.b * x10 + p11.b * x11 + p12.b * x12 + p13.b * x13 + p14.b * x14 + p15.b * x15 ) / ( 16 * 255 ) ) };
+				// load pixels
+				const __m128i row0 = _mm_load_si128( pRow0 );
+				const __m128i row1 = _mm_load_si128( pRow1 );
+				const __m128i row2 = _mm_load_si128( pRow2 );
+				const __m128i row3 = _mm_load_si128( pRow3 );
+
+				// process rows and sum results
+				__m128i sum = ProcessRow( row0 );
+				sum = _mm_add_epi16( sum,ProcessRow( row1 ) );
+				sum = _mm_add_epi16( sum,ProcessRow( row2 ) );
+				sum = _mm_add_epi16( sum,ProcessRow( row3 ) );
+
+				// add high and low pixel channel sums
+				sum = _mm_add_epi16( sum,_mm_srli_si128( sum,8u ) );
+
+				// divide channel sums by 256
+				sum = _mm_srli_epi16( sum,8u );
+
+				// pack word channels to bytes and store in output buffer
+				*pOut = _mm_cvtsi128_si32( _mm_packus_epi16( sum,sum ) );
 			}
 		}
 	}
