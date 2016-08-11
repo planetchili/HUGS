1475badd2017c56ff5e2313f3cf0a65c735212be
planetchili <planetchili@users.noreply.github.com>
2016-08-11 23:57:25 +0900

    first try for multiple phases

--- Engine/IntroScreen.h
+++ Engine/IntroScreen.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "Screen.h"
 #include "InputSystem.h"
-#include "ChiliMath.h"
 #include "TitleScreen.h"
+#include "TexturedQuad.h"
+#include "Viewport.h"
 #include "Midi.h"
 
 class IntroScreen : public Screen
@@ -14,26 +15,30 @@ public:
 		input( input ),
 		gfx( gfx ),
 		uranusPic( Surface::FromFile( L"uranus.png" ) ),
-		introMore( L"blt.mid",0.0f,30.0f )
+		introMore( L"blt.mid",0.0f,30.0f ),
+		hitler( L"hitler_blk.png" ),
+		awesome( L"hitawe.png" ),
+		port( gfx,{ 0.0f,float( gfx.GetHeight() - 1u ),0.0f,float( gfx.GetWidth() - 1u ) },
+			gfx.GetViewRegion() )
 	{
 	}
 	virtual void Update( float dt ) override
 	{
 		time += dt;
-		switch( stage )
+		switch( ss )
 		{
-		case Stage::Wait:
+		case SubStage::Wait:
 			if( time > waitTime )
 			{
-				stage = Stage::FadeIn;
+				ss = SubStage::FadeIn;
 				time = 0.0f;
 				introMore.Play();
 			}
 			break;
-		case Stage::FadeIn:
+		case SubStage::FadeIn:
 			if( time > fadeInTime )
 			{
-				stage = Stage::Hold;
+				ss = SubStage::Hold;
 				time = 0.0f;
 				alpha = 1.0f;
 			}
@@ -42,17 +47,27 @@ public:
 				alpha = time / fadeInTime;
 			}
 			break;
-		case Stage::Hold:
+		case SubStage::Hold:
 			if( time > holdTime )
 			{
-				stage = Stage::FadeOut;
+				ss = SubStage::FadeOut;
 				time = 0.0f;
 			}
 			break;
-		case Stage::FadeOut:
+		case SubStage::FadeOut:
 			if( time > fadeOutTime )
 			{
-				ChangeScreen( std::make_unique<TitleScreen>( gfx,input,parent ) );
+				if( ms == MasterStage::Hitler )
+				{
+					ChangeScreen( std::make_unique<TitleScreen>( gfx,input,parent ) );
+				}
+				else
+				{
+					ms = MasterStage::Hitler;
+					ss = SubStage::Wait;
+					time = 0.0f;
+					alpha = 0.0f;
+				}
 			}
 			else
 			{
@@ -66,7 +81,19 @@ public:
 	}
 	virtual void DrawPostBloom( D3DGraphics& gfx ) override
 	{
-		gfx.CopySurfaceViewRegion( uranusPic );
+		switch( ms )
+		{
+		case MasterStage::Uranus:
+			gfx.CopySurfaceViewRegion( uranusPic );
+			break;
+		case MasterStage::Hitler:
+		{
+			auto hd = hitler.GetDrawable();
+			hd.Transform( Mat3::Translation( { 900.0f,600.0f } ) );
+			port.Draw( hd );
+		}
+			break;
+		}
 		gfx.Fade( unsigned char( 31.9f * alpha ) * 8u );
 	}
 	virtual void HandleInput() override
@@ -77,23 +104,32 @@ public:
 		}
 	}
 private:
-	enum class Stage
+	enum class SubStage
 	{
 		Wait,
 		FadeIn,
 		Hold,
 		FadeOut
 	};
+	enum class MasterStage
+	{
+		Uranus,
+		Hitler
+	};
 private:
 	D3DGraphics& gfx;
 	InputSystem& input;
+	Viewport port;
 	Surface uranusPic;
+	TexturedQuad hitler;
+	TexturedQuad awesome;
 	float time = 0.0f;
-	Stage stage = Stage::Wait;
+	SubStage ss = SubStage::Wait;
+	MasterStage ms = MasterStage::Uranus;
 	MidiSong introMore;
 	float alpha;
-	static constexpr float waitTime = 3.0f;
-	static constexpr float fadeInTime = 4.0f;
-	static constexpr float holdTime = 0.8f;
-	static constexpr float fadeOutTime = 1.2f;
+	static constexpr float waitTime = 1.0f;
+	static constexpr float fadeInTime = 3.8f;
+	static constexpr float holdTime = 1.5f;
+	static constexpr float fadeOutTime = 1.0f;
 };
